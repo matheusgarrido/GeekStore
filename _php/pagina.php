ff433636dd1efb700188b7cb7a397be88cfec96e
Matheus Garrido <matheusgarrido10@hotmail.com>
2019-11-17 14:37:17 -0300

    Arquivo página

--- _php/pagina.php
+++ _php/pagina.php
@@ -1,5 +1,10 @@
 <?php
 
+define("USER", "root");
+define("PASS", "");
+define("BANCO", "geekstore");
+define("SITE", "http://localhost/geekstore/");
+
 class Pagina{
     function __construct($titulo){
         $this->htmlHead($titulo);
@@ -17,9 +22,10 @@ class Pagina{
                 <meta charset="utf-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <title>Geek Store  </title>
+                <title><?php echo $titulo; ?></title>
 
                 <!-- Google Fonts -->
+                <link rel="shortcut icon" href="_imagem/_logo/geekstore.png" >
                 <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
                 <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
                 <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
@@ -46,6 +52,89 @@ class Pagina{
         <?php
     }
     
+    function htmlCarrosselProdutos(){
+        ?>
+        <div class="slider-area">
+            <div class="block-slider block-slider4">
+                <ul class="" id="bxslider-home4">
+                    <li><img src="img/h4-slide.png" alt="Slide">
+                        <div class="caption-group">
+                            <h2 class="caption title">
+                                Camiseta de <br> <span class="primary"> Harry Potter <br> da <strong> Grifinória <br></strong></span> Dupla Face
+                            </h2>
+                            <!--<h4 class="caption subtitle"></h4>-->
+                            <a class="caption button-radius" href="#"><span class="icon"></span>Detalhes do produto</a>
+                        </div>
+                    </li>
+                    <li><img src="img/h4-slide2.png" alt="Slide">
+                        <div class="caption-group">
+                            <h2 class="caption title">
+                                Jaqueta <span class="primary"> Bomber <br> <strong> Frank Castle </strong></span>
+                            </h2>
+                            <!--<h4 class="caption subtitle">school supplies & backpacks.*</h4>-->
+                            <a class="caption button-radius" href="#"><span class="icon"></span>Detalhes do produto</a>
+                        </div>
+                    </li>
+                    <li><img src="img/h4-slide3.png" alt="Slide">
+                        <div class="caption-group">
+                            <h2 class="caption title">
+                                    Par de meias de <br> <span class="primary"><strong> Avengers </strong> da Marvel</span>
+                            </h2>
+                            <!--<h4 class="caption subtitle">Select Item</h4>-->
+                            <a class="caption button-radius" href="#"><span class="icon"></span>Detalhes do produto</a>
+                        </div>
+                    </li>
+                    <li><img src="img/h4-slide4.png" alt="Slide">
+                        <div class="caption-group">
+                            <h2 class="caption title">
+                                Camisa <span class="primary"> uniforme da <br><strong> Capitã Marvel</strong><br> </span> com manga
+                            </h2>
+                            <!--<h4 class="caption subtitle">& Phone</h4>-->
+                            <a class="caption button-radius" href="#"><span class="icon"></span>Detalhes do produto</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <?php
+    }
+    
+    function htmlCardsServicos(){
+        ?>
+        <div class="promo-area">
+            <div class="zigzag-bottom"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="single-promo promo1">
+                            <i class="fa fa-refresh"></i>
+                            <p>30 dias para troca</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="single-promo promo2">
+                            <i class="fa fa-truck"></i>
+                            <p>Frete grátis</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="single-promo promo3">
+                            <i class="fa fa-lock"></i>
+                            <p>Pagamentos seguros</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="single-promo promo4">
+                            <i class="fa fa-gift"></i>
+                            <p>Produtos exclusivo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    
     function htmlCabecalho(){
         ?>
         <body>
@@ -54,7 +143,7 @@ class Pagina{
                     <div class="row">
                         <div class="col-sm-6">
                             <div class="logo">
-                                <h1><a href="./"><img src="img/logo.png"></a></h1>
+                                <h1><a href="index.php"><img src="_imagem/_logo/geekstore.png" width="150"></a></h1>
                             </div>
                         </div>
 
@@ -80,7 +169,7 @@ class Pagina{
                         </div> 
                         <div class="navbar-collapse collapse">
                             <ul class="nav navbar-nav">
-                                <li class="active"><a href="index.html">Início</a></li>
+                                <li class="active"><a href="index.php">Início</a></li>
                                 <li><a href="shop.html">Produtos</a></li>
                                 <li><a href="single-product.html">Marcas</a></li>
         <!--                        <li><a href="cart.html">Cart</a></li>
@@ -96,9 +185,6 @@ class Pagina{
         <?php
     }
     
-    function htmlBody(){
-        
-    }
     function htmlFooter(){
         ?>
             <div class="footer-top-area">
@@ -110,10 +196,10 @@ class Pagina{
                                 <h2><span>Geek</span>Store</h2>
                                 <p>Geek Store é a menor loja de todo o Brasil, com diversas roupas exclusivas de diversos filmes, jogos, séries, desenhos e animes.</p>
                                 <div class="footer-social">
-                                    <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                                    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                                    <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
-                                    <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://www.youtube.com/?hl=pt&gl=BR" target="_blank"><i class="fa fa-youtube"></i></a>
+                                    <a href="https://br.linkedin.com/" target="_blank"><i class="fa fa-linkedin"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -122,10 +208,10 @@ class Pagina{
                             <div class="footer-menu">
                                 <h2 class="footer-wid-title">Menu de Navegação</h2>
                                 <ul>
-                                    <li><a href="#">Página principal</a></li>
-                                    <li><a href="#">Produtos</a></li>
-                                    <li><a href="#">Marcas</a></li>
-                                    <li><a href="#">Contato</a></li>
+                                    <li><a href="<?php echo SITE; ?>">Página principal</a></li>
+                                    <li><a href="<?php echo SITE."catalogo.php"; ?>">Produtos</a></li>
+                                    <li><a href="<?php echo SITE."marcas.php"; ?>">Marcas</a></li>
+                                    <li><a href="<?php echo SITE."contato.php"; ?>">Contato</a></li>
                                 </ul>                        
                             </div>
                         </div>
@@ -134,10 +220,10 @@ class Pagina{
                             <div class="footer-menu">
                                 <h2 class="footer-wid-title">Marcas</h2>
                                 <ul>
-                                    <li><a href="#">Marvel</a></li>
-                                    <li><a href="#">DC</a></li>
-                                    <li><a href="#">Star Wars</a></li>
-                                    <li><a href="#">Harry Potter</a></li>
+                                    <li><a href="<?php echo SITE."catalogo.php?marca=Marvel"; ?>">Marvel</a></li>
+                                    <li><a href="<?php echo SITE."catalogo.php?marca=DC"; ?>">DC</a></li>
+                                    <li><a href="<?php echo SITE."catalogo.php?marca=Star%20Wars"; ?>">Star Wars</a></li>
+                                    <li><a href="<?php echo SITE."marcas.php"; ?>">Outras marcas</a></li>
                                 </ul>                        
                             </div>
                         </div>
@@ -190,6 +276,32 @@ class Pagina{
         </body>
     </html>
         <?php
+    }
+    
+    function sqlSelect($selectQuery){
+        $connect = mysqli_connect(HOST, USER, PASS, BANCO) or die('Não foi possível conectar no banco.');
+        $result = mysqli_query($connect, $selectQuery);
+        $vetor = array();
+    //    echo $selectQuery."<br>";
+        //Gerando array da Área
+    //    var_dump($result);
+        if (mysqli_num_rows($result)>0){
+            while ($row = mysqli_fetch_assoc($result)){
+    //            var_dump($row);
+                $vetor[]=$row;
+    //            var_dump($vetor);
+            }
+        }
+    //    print_r($vetor);
+    //    var_dump($vetor);
+        mysqli_close($connect);
+    //    var_dump($vetor);
+        return $vetor;
+    }
+    
+    function nomeLink($nome){
+        $nomeLink = $nome;
+        return $nomeLink;
     }
 }
 ?>
